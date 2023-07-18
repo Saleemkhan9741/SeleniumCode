926e43788f4b881e9aa8fae867629bbb2d1d8da5
Saleemkhan9741 <saleemkhan9741@gmail.com>
2023-07-18 19:21:55 +0530

    added reporting

--- src/test/java/org/selenium/amazon/BaseTest.java
+++ src/test/java/org/selenium/amazon/BaseTest.java
@@ -10,13 +10,21 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
 public class BaseTest {
 
     private static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
 
     @Parameters({"browser","env","headlessEnabled"})
     @BeforeSuite(groups = {"regression","smoke","sanity"})
-    public void setUp(String browser, String env, boolean headlessEnabled){
+    public void setUp(String browser, String env, boolean headlessEnabled) throws IOException {
+        archivePreviousResult();
         LOGGER.info("Setting the environment");
         PropertyReader.environment = env;
         LOGGER.info("Current environment is set to  :{}", env);
@@ -34,4 +42,23 @@ public class BaseTest {
         LOGGER.info("Closing the driver");
         DriverManager.getInstance().closeDriver();
     }
+
+    private void archivePreviousResult() throws IOException {
+        String dirPath = System.getProperty("user.dir") + File.separator + "reports";
+        boolean previousFileExists = new File(dirPath).exists();
+        if(previousFileExists){
+            Path dir = Paths.get(dirPath);
+            Files.walk(dir)
+                    .sorted(Comparator.reverseOrder())
+                    .forEach(path -> {
+                        try {
+                            System.out.println("Deleting: " + path);
+                            Files.delete(path);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
 }
